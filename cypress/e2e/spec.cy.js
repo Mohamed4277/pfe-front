1dbcc5f10d7eb74c148c33880a3e25eb9b48dcfc
Mai <mohamed.aissaoui.perso@gmail.com>
2022-11-27 21:38:23 +0100

    update e2e test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
-    cy.get('#userName').type('b@b.fr')
+    cy.get('#userName').type('mai@mai.fr')
     cy.get('#password').type('12')
     cy.get('#submit-to-enter').click()
     cy.get('#add-item').click()
